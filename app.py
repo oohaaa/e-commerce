c60017f79aaaf7479639beae5b12534a1289a320
Ooha Chekuri <137389869+oohaaa@users.noreply.github.com>
2025-07-15 20:15:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Ooh@ 429",
+        password="password",
         database="ecommerce"
     )
 
